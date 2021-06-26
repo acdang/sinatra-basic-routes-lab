3eff1fcea6eaf357902da6b310f2552f26bd096f
Anh Dang <anhcdang@gmail.com>
2021-06-26 15:45:26 -0700

    commit tree 5d23375ac7419e6309dcc3c585505b9f8688b9c7

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+      end 
+
+    get '/name' do
+        "My name is Anh."
+    end
+
+    get '/hometown' do
+        "My hometown is San Jose, CA."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Reverie."
+    end
 end
